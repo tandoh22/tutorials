f12fccac0a5366b4bfbd64e309e839bb99d731ef
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-04 12:36:46 +0000

    If statements

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-import math
+name = input('what is your name? ')
+birth_year = int(input('what is your birth year? '))
+age = 2025 - birth_year
+print(f"you are {age} years old now")
 
-a = float(input('enter the value of the adjacent: '))
-b = float(input('enter the value of the opposite: '))
-hypotanus = math.sqrt(pow(a, 2) + pow(b, 2))
-print(f"the hypotanuus is {hypotanus}cm")
+weight = float(input('how much do you weigh? '))
+if weight >= 200:
+    print('damn! you are obolo')
+    print('you need to work out asap!!!')
+else:
+    print('your weight is normal')
+    print('have a nice a day!!!!!!!')    
