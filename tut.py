4abca2244cccc57166411f55dda1eec0feeb7bcb
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-12 09:10:19 +0000

    new

--- tut.py
+++ tut.py
@@ -1,11 +1,12 @@
 # for loop
 
+import math
 import time
-my_time = int(input("enter the time in seconds: "))
-for x in range(my_time, 0, -1 ):
+
+radius = float(input('what is the radius of the circle? '))
+area = math.pi * radius ** 2
+time = 3
+for x in range(time, 0, -1):
     seconds = x % 60
-    minutes = int(x / 60) % 60
-    hours = int(x / 3600) % 60
-    print(f"00:{minutes:02}:{seconds:02}")
-    time.sleep(1)
-print("Time is up!")
+    print(f"00:00:{seconds}")
+print(f"in 3 seconds, the area of the circle is {area}m*2")
