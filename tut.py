587363170e1580c13e484f00e703eda7d5572474
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-12 08:34:52 +0000

    new

--- tut.py
+++ tut.py
@@ -2,8 +2,8 @@
 
 import time
 my_time = int(input("enter the time in seconds: "))
-for x in range(my_time, 0, -1):
+for x in range(my_time ):
     seconds = x % 60
-    print(f"00:00:{seconds}")
+    print(f"00:00:{seconds:02}")
     time.sleep(1)
 print("Time is up!")
