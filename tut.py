17bdf596e5d17b4bff28ca26e079c74590a44757
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-12 10:10:52 +0000

    new

--- tut.py
+++ tut.py
@@ -1,13 +1,6 @@
 # for loop
 
-import math
-import time
+names = {"joshua", "harriet", "samuel", "melo"}
 
-radius = float(input('what is the radius of the circle? '))
-area = math.pi * radius ** 2
-sec = 3
-for x in range(sec, 0, -1):
-    seconds = x % 60
-    print(f"00:00:{seconds:02}")
-    time.sleep(1)
-print(f"in 3 seconds, the area of the circle is {area}m*2")
+#print(dir(names))
+print(names)
