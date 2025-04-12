59340d728f333645f89391745de088419d32760a
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-12 09:17:07 +0000

    new

--- tut.py
+++ tut.py
@@ -5,8 +5,9 @@ import time
 
 radius = float(input('what is the radius of the circle? '))
 area = math.pi * radius ** 2
-time = 3
-for x in range(time, 0, -1):
+sec = 3
+for x in range(sec, 0, -1):
     seconds = x % 60
-    print(f"00:00:{seconds}")
+    print(f"00:00:{seconds:02}")
+    time.sleep(1)
 print(f"in 3 seconds, the area of the circle is {area}m*2")
