4953a08cda34686bb8bac189cca3ccc31e63d625
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-17 12:26:20 +0000

    new

--- tut.py
+++ tut.py
@@ -7,6 +7,15 @@ while True:
     if shirt.lower == "e":
         break
     else:
-        price = float(input(f"enter the price of the {shirt}"))
+        price = float(input(f"enter the price of the {shirt}: $"))
         shirts.append(shirt)
         prices.append(price)
+
+print("<<<<<< YOUR CART >>>>>>")
+
+for shirt in shirts:
+    print(shirt)
+for price in prices:
+    total += prices
+print()
+print(f"the total cost of your items is: ${total}")
