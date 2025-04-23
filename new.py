fb17ee7e07e37c6937fd4fdf8a7b2079b5aa22c3
tandoh22 <tandohsamuel4u@gmail.com>
2025-04-23 10:20:03 +0000

    new

--- new.py
+++ new.py
@@ -3,4 +3,11 @@ brand = {"Lacoste": 120,
          "luis vuiton": 180,
          "gucci": 120,
          "balenciaga": 400,
-         }
+         "casablanca": 100,
+         "off-white": 150,
+         "versace": 320}
+cart = []
+total = 0
+
+for key, value in brand.items():
+    print(f"{key} {value}")
