68aa8f0a823538f824e2e9153b7e0eb241c1dbb1
tandoh22 <tandohsamuel4u@gmail.com>
2025-05-02 15:23:23 +0000

    new

--- num.py
+++ num.py
@@ -8,3 +8,14 @@ is_running = True
 
 print('Python Number Guessing Game')
 print(f"Select a number between {lowest_num} and {highest_num}")
+
+while is_running:
+    guess = input('Enter your guess: ')
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+        if guess < {lowest_num} or guess > {highest_num}:
+            print("Your guess is out of range")
+    else:
+        print("INVALID GUESS!")
+        print(f"Please select a number between {lowest_num} and {highest_num}")
