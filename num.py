c122ba7fdd31a45b28cb4813183c3a070e0dfedc
tandoh22 <tandohsamuel4u@gmail.com>
2025-05-05 15:08:05 +0000

    new

--- num.py
+++ num.py
@@ -14,6 +14,18 @@ while is_runnung:
     if guess.isdigit():
         guess = int(guess)
         guesses += 1
+        if guess < lowest_num or guess > highest_num:
+            print("Your guess is out of range")
+            print(f"Please select a number between {lowest_num} and {highest_num}")
+        elif guess < answer:
+            print("Too low, try again!")
+        elif guess > answer:
+            print("Too high, try again!")
+        else:
+            print(f"CORRECT!, you guessed right")
+            print(f"the right answer is {answer}")
+            print(f"Number of guesses: {guesses}")
+            is_runnung = False
     else:
         print('INVALID GUESS')
-        print(f"Please select a number between {lowest_num}")
+        print(f"Please select a number between {lowest_num} and {highest_num}")
