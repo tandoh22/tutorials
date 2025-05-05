554b9c3ae0cd7a8290b5a5b06ca2806b7f79d9c1
tandoh22 <tandohsamuel4u@gmail.com>
2025-05-05 14:18:32 +0000

    new

--- num.py
+++ num.py
@@ -6,3 +6,14 @@ guesses = 0
 answer = random.randint(lowest_num, highest_num)
 is_runnung = True
 
+print('Number guessing game')
+print(f"Select a number between {lowest_num} and {highest_num}")
+
+while is_runnung:
+    guess = input('Enter your guess: ')
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+    else:
+        print('INVALID GUESS')
+        print(f"Please select a number between {lowest_num}")
