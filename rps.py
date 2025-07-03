641a3b9555bd4b1fe643acb73db1a1b3dd4be4f1
tandoh22 <tandohsamuel4u@gmail.com>
2025-07-03 16:57:20 +0000

    new

---
+++ rps.py
@@ -0,0 +1,6 @@
+import random
+
+options = ("rock", "paper", "scissors")
+player = None
+computer = random.choice(options)
+
--- tut.py
+++ tut.py
@@ -1,22 +1,6 @@
-shirts = []
-prices = []
-total = 0 
+def birthday(name, age):
+    print(f"happy birthday to {name}")
+    print(f"you are {age} years old")
+    print("May God bless you!")
 
-while True:
-    shirt = input("enter a brand to buy (e to exit): ")
-    if shirt.lower() == "e":
-        break
-    else:
-        price = float(input(f"enter the price of {shirt}: $"))
-        shirts.append(shirt)
-        prices.append(price)
-
-print("<<<<<< YOUR CART >>>>>>") 
-
-for shirt in shirts:
-    print(shirt, end=" ")
-for price in prices:
-    total += price
-print()
-print(f"the total cost of your items is: ${total}")
-print("THANK YOU!")
+birthday("Bevelyn", 18)
