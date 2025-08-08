4b59b10108225a9e973e3f0cb5d3a140ee124bab
tandoh22 <tandohsamuel4u@gmail.com>
2025-08-08 17:11:32 +0000

    NEW

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void main()
+        {
+            Console.WriteLine("Enter your score (0 - 100): ");
+            double score = convert.Todouble(Console.ReadLine());
+            if (score < 0 || score > 100)
+            {
+                Console.WriteLine("Invalid score. Please enter a value between 0 and 100.");
+            }
+            string grade;
+            if (score >= 90)
+            {
+                grade = "A";
+            }
+            else if (score >= 80)
+            {
+                grade = "B";
+            }
+            else if (score >= 70)
+            {
+                grade = "C"
+            }
+        }
+    }
+}
